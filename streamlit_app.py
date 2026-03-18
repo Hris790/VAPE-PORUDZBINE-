85bb33eb1655bc378e8807f048f1e9071ffd6c7d
Hris790 <hristinaap003@gmail.com>
2026-03-18 09:29:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2157,7 +2157,7 @@ elif page == 'mesecni':
             return None
         if buf_t is None:
             return None
-        iskljuci_poslednji = True
+        iskljuci_poslednji = False
 
         df = pd.read_excel(buf_s, sheet_name='tabela')
         df.columns = df.columns.astype(str).str.strip()
@@ -2644,7 +2644,7 @@ elif page == 'finansijski':
         buf_s = load_github_excel("tabela sistemi3.xlsx")
         if buf_s is None:
             return None
-        ukljuci_poslednji = False
+        ukljuci_poslednji = True
 
         df = pd.read_excel(buf_s, sheet_name='tabela')
         df.columns = df.columns.astype(str).str.strip()
